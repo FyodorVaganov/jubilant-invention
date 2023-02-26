eebe680284adebb420aa171b6704f89221b2c371
FyodorVaganov <75779701+FyodorVaganov@users.noreply.github.com>
2023-02-26 12:56:46 +0300

    Update not_main.py
    ball is added to the game

--- not_main.py
+++ not_main.py
@@ -1,5 +1,18 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
 back = (200, 255, 255)
 win_width = 600
 win_height = 500
@@ -11,9 +24,13 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if not finish:
+        ball.reset()
     display.update()
     clock.tick(FPS)
