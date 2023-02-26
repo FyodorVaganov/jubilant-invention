8a8b800d9ab898a2e48b26fd7f6348352ffb5c05
FyodorVaganov <75779701+FyodorVaganov@users.noreply.github.com>
2023-02-26 13:04:02 +0300

    Update not_main.py
    adding players in the project

--- not_main.py
+++ not_main.py
@@ -12,6 +12,20 @@ class GameSprite(sprite.Sprite):
 
    def reset(self):
        window.blit(self.image, (self.rect.x, self.rect.y))
+    
+class Player(GameSprite):
+   def update_r(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+   def update_l(self):
+       keys = key.get_pressed()
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
 
 back = (200, 255, 255)
 win_width = 600
@@ -24,6 +38,8 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
 while game:
@@ -31,6 +47,12 @@ while game:
         if e.type == QUIT:
             game = False
     if not finish:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
         ball.reset()
     display.update()
     clock.tick(FPS)
